45da5bdee80ff541310ba70c2c3940d8c803ed53
Nicolas Theau <theaun91@gmail.com>
2024-04-23 15:09:37 +0200

    Création back

---
+++ back/src/schema.ts
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+
+  type Query {
+    getUser: [User]!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  # type Mutation {
+  #   # createUser(username: String!, password: String!): CreateUserResponse!
+  #   # signIn(username: String!, password: String!): SignInResponse!
+  # }
+
+  type CreateUserResponse {
+    code: Int!
+    message: String!
+    success: Boolean!
+    user: User
+  }
+
+  type SignInResponse {
+    code: Int!
+    message: String!
+    success: Boolean!
+    token: String
+    user: User
+  }
+`;
