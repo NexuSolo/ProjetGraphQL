8f071eadf29ad55ac748526c116ae30e1a530cad
Yanis Rozier <yanis.rozier@orange.fr>
2024-04-26 14:59:42 +0200

    design front finis

--- front/src/components/CreationArticle.vue
+++ front/src/components/CreationArticle.vue
@@ -4,10 +4,11 @@
         <h1>Creer un article</h1>
 
         <!-- textarea pour rediger -->
-        <div>
-            <textarea id="article" v-model="article" required></textarea>
+        <div class="content">
+            <input class="titre" v-model="titre" placeholder="Titre de l'article" required>
+            <textarea class="contenu" v-model="article" placeholder="Contenu de l'article" required></textarea>
             <!-- publier -->
-            <button type="submit">Publier</button>
+            <button class="button-submit" type="submit">Publier</button>
         </div>
     
     </div>
@@ -20,9 +21,47 @@
 
 <style scoped>
 
-textarea {
+textarea, input {
     width: 80vw;
+    resize: none;
+    border-radius: 10px;
+    border : none;
+    background-color: rgb(233, 233, 233);
+    font-size: 1.5vh;
+    padding: 1vw;
+    margin: 2vh;
+    font-family: Arial, sans-serif;
+}
+
+textarea {
     height: 50vh;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.button-submit {
+    background-color: #67aa69;
+    color: white;
+    padding: 14px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 2vh;
+    padding-top: 1.5vh;
+    padding-bottom: 1.5vh;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    transition: 0.2s;
+}
+
+.button-submit:hover {
+    background-color: #79be7c;
 }
 
 </style>
--- front/src/components/VisionnerArticle.vue
+++ front/src/components/VisionnerArticle.vue
@@ -1,14 +1,21 @@
 <template>
 
     <div>
-        <h1>Titre du chapitre</h1>
-        <p>Contenu du chapitre</p>
+        <h1>Titre de l'article</h1>
+        <p class="article-contenu">Contenu de l'article</p>
         <!-- likes -->
-        <button type="submit">J'aime</button>
         <!-- commentaires -->
-        <div>
-            <textarea id="commentaire" v-model="commentaire" required></textarea>
-            <button type="submit">Commenter</button>
+        <div class="react">
+            <button class="like" type="submit"><img src="../assets/like.png" alt=""></button>
+            <textarea class="texte-commenter" v-model="commentaire" placeholder="Ecrire un commentaire ..." required></textarea>
+            <button class="button-commenter" type="submit"><img class="img-commentaire" src="../assets/commenter.png" alt=""></button>
+        </div>
+
+        <div class="liste-commentaires">
+            <div class="commentaire">
+                <h4 class="commentaire-auteur">Test</h4>
+                <p class="commentaire-texte">Commentaire</p>
+            </div>
         </div>
     </div>
 
@@ -18,6 +25,122 @@
 
 </script>
 
-<style>
+<style scoped>
+
+.liste-commentaires {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 47vw;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: rgb(233, 233, 233);
+    padding: 1vw;
+    border-radius: 10px;
+}
+
+.commentaire {
+    width: 95%;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 1.5vh;
+    font-family: Arial, sans-serif;
+    background-color: rgb(245, 245, 245);
+    padding: 1vw;
+    border-radius: 10px;
+    text-align: justify;
+    margin-top: 0.5vh;
+    margin-bottom: 0.5vh;
+}
+
+.commentaire-auteur {
+    font-weight: bold;
+    margin: 0;
+    padding: 0;
+}
+
+.commentaire-texte {
+    margin: 0;
+    padding: 0.2vh;
+}
+
+.article-contenu {
+    width: 47vw;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 1.5vh;
+    font-family: Arial, sans-serif;
+    background-color: rgb(233, 233, 233);
+    padding: 1vw;
+    border-radius: 10px;
+    text-align: justify;
+}
+
+.react{
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+    width: 53vw;
+
+}
+
+.like {
+    background-color: rgb(233, 233, 233);
+    color: white;
+    padding: 14px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 10px;
+    padding-top: 1.5vh;
+    padding-bottom: 1.5vh;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    transition: 0.2s;
+    margin: 2vw;
+}
+
+.like:hover {
+    background-color: #d8b3b3;
+}
+
+textarea, input {
+    width: 50vw;
+    resize: none;
+    border-radius: 10px;
+    border : none;
+    background-color: rgb(233, 233, 233);
+    font-size: 1.5vh;
+    padding: 1vw;
+    margin: 2vh;
+    font-family: Arial, sans-serif;
+}
+
+.button-commenter {
+    background-color: rgb(233, 233, 233);
+    padding: 14px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 10px;
+    padding-top: 1.5vh;
+    padding-bottom: 1.5vh;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    transition: 0.2s;
+    margin: 2vw;
+}
+
+.button-commenter:hover {
+    background-color: #cecece;
+}
+
+
+img{
+    width: 2vw;
+    height: 2vw;
+    opacity: 0.7;
+}
+
+
 
 </style>
